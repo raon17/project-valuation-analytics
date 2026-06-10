fe27f5eab68f894066c4bb47cbbd81cfaf5b341e
William Kim <willkim0117@gmail.com>
2026-06-10 22:36:54 +1000

    Import info from yf.ticker

--- app.py
+++ app.py
@@ -13,3 +13,28 @@ def valuation(eps, growth, pe, years, discount=0.15, mos=0.5):
 ticker = st.text_input("Ticker", "NVDA").upper()
 
 years = st.slider("Projection Years", 5, 15, 10)
+
+if ticker:
+
+    stock = yf.Ticker(ticker)
+    info = stock.info
+
+    price = (
+        info.get("currentPrice")
+        or info.get("regularMarketPrice")
+        or info.get("previousClose")
+    )
+
+    eps = info.get("trailingEps") or info.get("forwardEps") or 1
+
+    growth = info.get("earningsGrowth")
+    growth = growth if growth else 0.15
+
+    pe = info.get("trailingPE") or info.get("forwardPE") or 20
+
+    intrinsic, fair_value = valuation(
+        eps=eps,
+        growth=growth,
+        pe=pe,
+        years=years
+    )
